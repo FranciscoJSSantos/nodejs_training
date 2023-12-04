de2f979730abc8a1976c8d523897c40c5d6d6294
FRANCISCO JOSE DOS SANTOS SANTANA <francisco.a.santana@capgmemini.com>
2023-12-04 15:58:33 -0300

    MODULO 6 - param-url

---
+++ 6_EXPRESS/6_param_url/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const port = 3000 //variavel de ambiente
+
+const path = require('path')
+
+const basePath = path.join(__dirname, 'templates')
+
+app.get('/users/:id',(req, res) => {
+   const id = req.params.id
+
+   //leitura da tabela users, resgatar um usuario
+   console.log(`Estamos buscando pelo usuário ${id}`)
+
+   res.sendFile(`${basePath}/index.html`)
+})
+
+app.get('/',(req, res) => {
+   res.sendFile(`${basePath}/index.html`)
+})
+
+
+app.listen(port, () => {
+   console.log(`App rodando na porta ${port} 🟢`)
+})
+
+
